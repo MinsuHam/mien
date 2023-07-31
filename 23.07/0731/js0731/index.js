4bcfe104e9ebab6126dd5b91d2abf5b3640fdaf8
Minsu Ham <minsu4867@naver.com>
2023-07-31 22:20:49 +0900

    0731

---
+++ 23.07/0731/js0731/index.js
@@ -0,0 +1,27 @@
+// let txt = "Hello Thank you good luck to you";
+// // let t = txt.split(" ")
+// // for(let i = 0; i < t.length; i++){
+// //     console.log(t[i] + " " + i);
+// // }
+
+// let t = txt.charCodeAt(0);
+// console.log(t); // A = 65(아스키 코 드값), H = 72
+// console.log(String.fromCharCode(65)); // A
+
+//console.log(txt.charAt(16));
+//console.log("--------------------1------------------");
+//console.log(txt.indexOf("you")); console.log(txt.lastIndexOf("you"));
+// console.log(txt.indexOf("Hi")); console.log(txt.match("Hi"));
+// console.log(txt.search("Hi")); indexOf와 같이 -1 반환
+// console.log("--------------------2------------------");
+// console.log(txt.replaceAll("you", "chulsoo"));  you문자 전부 바꿈(cf. replace)
+
+// for (let i = 0; i < txt.length; i++) {
+//     console.log(txt[i] + i);
+// }
+//substr 첫번쨰가 시작 인덱스 번호, 두 번째가 길이
+// console.log(txt.substr(21, 4));
+// console.log(txt.substring(6, 12)); // 첫 번째 인덱스부터 두번째 인덱스 이전까지 =Thank
+// console.log(txt.slice(6, 12)); // Thank
+
+
---
+++ 23.07/0731/web0731/js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    //우리의 스크립트 코드
+});
