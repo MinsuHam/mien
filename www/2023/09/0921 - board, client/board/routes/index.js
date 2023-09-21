d7e7445bb8cd6b7d71a6dae503afacd81a7214a4
Minsu Ham <minsu4867@naver.com>
2023-09-21 21:16:44 +0900

    2023.09.21 commit

---
+++ www/2023/09/0921 - board, client/board/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+//기본 주소 설정
+router.get('/', (req, res) => {
+    res.render('index', { title: "게시판 목록"});
+});
+
+router.get("/write", (req, res)=>{
+    res.render("write", { title: "게시판 글쓰기"});
+});
+
+router.get("/view", (req, res)=>{
+   res.render("view", { title: "게시판 내용보기"});
+});
+
+module.exports = router;
